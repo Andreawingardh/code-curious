dc187e9852f18b26b1c8bf4cd41eefcfe902b248
Skjesper <jesperskeppstedt@gmail.com>
2025-09-10 11:06:34 +0200

    Add cancel button to delete confirmation modal
    A cancel button was added to the DeleteConfirmationModal component to allow users to close the modal without confirming the delete action.

--- src/components/DeleteConfirmationModal.svelte
+++ src/components/DeleteConfirmationModal.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
+
 	import Modal from './Modal.svelte';
 
       export let deleteConfirmation: boolean = false;
@@ -16,6 +17,8 @@
 	<div slot="header" class="card-header flex justify-between items-start">
 		<div class="flex flex-col">TIME FOR DELETE</div>
         <button on:click={() => {deleteConfirmation = true}}>Delete</button>
+		<button on:click={() => {isOpen = false}}>Cancel</button>
+		
  
 	</div></Modal
 >
